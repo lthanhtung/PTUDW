482077da3f311b33492874da79f43d7c792ab886
Lê Thanh Tùng <130471358+lthanhtung@users.noreply.github.com>
2023-09-28 23:47:17 +0700

    ProjectBanHang_Day1

---
+++ ProjectBanHang/ProjectBanHang/Areas/Admin/Controllers/DasboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectBanHang.Areas.Admin.Controllers
+{
+    public class DasboardController : Controller
+    {
+        // GET: Admin/Dasboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
