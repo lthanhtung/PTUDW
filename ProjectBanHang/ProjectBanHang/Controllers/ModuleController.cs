5a5f47944bcbe30a8d592745ebf9512558c660e1
Lê Thanh Tùng <130471358+lthanhtung@users.noreply.github.com>
2023-11-24 15:41:35 +0700

    Project_BanHang_Ngay9

---
+++ ProjectBanHang/ProjectBanHang/Controllers/ModuleController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Myclass.DAO;
+using Myclass.Model;
+
+namespace ProjectBanHang.Controllers
+{
+    public class ModuleController : Controller
+    {
+        MenusDAO menusDAO = new MenusDAO();
+        // GET: Module
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MainMenu()
+        {
+            List<Menus> list = menusDAO.getListByParentId(0);
+
+            return View(list);
+        }
+
+    }
+    
+}
